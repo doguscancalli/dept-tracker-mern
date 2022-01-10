f6674aeaee0da059e040ffdb0ff20bb12ba9f326
Doğuş <dqampire@gmail.com>
2022-01-11 00:41:58 +0300

    Update api url

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -1,68 +1,75 @@
-import React, { createContext, useReducer } from "react";
-import AppReducer from "./AppReducer";
-import axios from "axios";
+import React, { createContext, useReducer } from 'react'
+import AppReducer from './AppReducer'
+import axios from 'axios'
 
 // Initial state
 const initialState = {
   isLoading: false,
-  currency: "$",
+  currency: '$',
   balance: 0,
   loansGiven: [],
   loansTaken: [],
   totalLoansGiven: 0,
   totalLoansTaken: 0,
-};
+}
 
 // Create context
-export const GlobalContext = createContext(initialState);
+export const GlobalContext = createContext(initialState)
 
 // Provider component
 export const GlobalProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(AppReducer, initialState);
+  const [state, dispatch] = useReducer(AppReducer, initialState)
 
   // Actions
   const setBalance = (payload) => {
     dispatch({
-      type: "SET_BALANCE",
+      type: 'SET_BALANCE',
       payload: payload,
-    });
-  };
+    })
+  }
   const getRecords = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/records");
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/records`
+      )
 
       dispatch({
-        type: "GET_RECORDS",
+        type: 'GET_RECORDS',
         payload: res.data.data,
-      });
+      })
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
   const addRecord = async (record) => {
     try {
-      const res = await axios.post("http://localhost:5000/api/records", record);
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/api/records`,
+        record
+      )
 
       dispatch({
-        type: "ADD_RECORD",
+        type: 'ADD_RECORD',
         payload: res.data.data,
-      });
+      })
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
   const deleteRecord = async (payload) => {
     try {
-      await axios.delete(`http://localhost:5000/api/records/${payload._id}`);
+      await axios.delete(
+        `${process.env.REACT_APP_API_URL}/api/records/${payload._id}`
+      )
 
       dispatch({
-        type: "DELETE_RECORD",
+        type: 'DELETE_RECORD',
         payload: payload,
-      });
+      })
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   return (
     <GlobalContext.Provider
@@ -76,5 +83,5 @@ export const GlobalProvider = ({ children }) => {
     >
       {children}
     </GlobalContext.Provider>
-  );
-};
+  )
+}
